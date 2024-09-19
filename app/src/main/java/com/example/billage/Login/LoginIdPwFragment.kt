3f64fc290e879d5961564dd1305401d1576faee8
Geonheu <geonhui0987@gmail.com>
2024-09-19 22:55:53 +0900

    [Feat]#8 비밀번호 눈 아이콘 비밀번호보이기 구현

--- app/src/main/java/com/example/billage/Login/LoginIdPwFragment.kt
+++ app/src/main/java/com/example/billage/Login/LoginIdPwFragment.kt
@@ -1,6 +1,8 @@
 package com.example.billage.Login
 
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,8 +19,7 @@ class LoginIdPwFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+        // toggleEye()
     }
 
     override fun onCreateView(
@@ -27,6 +28,8 @@ class LoginIdPwFragment : Fragment() {
     ): View? {
         _binding = FragmentLoginIdPwBinding.inflate(inflater, container, false)
         val view = binding.root
+        toggleMakePwEye()
+        toggleCheckPwEye()
         return view
     }
 
@@ -35,7 +38,7 @@ class LoginIdPwFragment : Fragment() {
         // Toolbar 설정
         val toolbar : Toolbar = binding.toolbarLogin
         toolbar.setTitle(R.string.toolbar_register)
-        toolbar.setNavigationIcon(R.drawable.icon_close_button)
+        toolbar.setNavigationIcon(R.drawable.icon_back_button)
         // Toolbar 뒤로가기
         toolbar.setNavigationOnClickListener {
             requireActivity().supportFragmentManager.popBackStack() // 뒤로가기
@@ -45,5 +48,45 @@ class LoginIdPwFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // 비밀번호 보이게하는 토글
+    fun toggleMakePwEye(){
+        val btnEye = binding.imageButtonMakePwEye
+        val editTextMakePw = binding.editTextMakePw
+        btnEye.setOnClickListener {
+            when(it.tag){
+                "0" -> {
+                    btnEye.tag = "1"
+                    editTextMakePw.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                    btnEye.setImageResource(R.drawable.icon_eye_off)
+                }
+                "1" -> {
+                    btnEye.tag = "0"
+                    editTextMakePw.transformationMethod = PasswordTransformationMethod.getInstance()
+                    btnEye.setImageResource(R.drawable.icon_eye_on)
+                }
+            }
+            editTextMakePw.setSelection(editTextMakePw!!.length())
+        }
+    }
+    fun toggleCheckPwEye(){
+        val btnEye = binding.imageButtonCheckPwEye
+        val editTextMakePw = binding.editTextCheckPw
+        btnEye.setOnClickListener {
+            when(it.tag){
+                "0" -> {
+                    btnEye.tag = "1"
+                    editTextMakePw.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                    btnEye.setImageResource(R.drawable.icon_eye_off)
+                }
+                "1" -> {
+                    btnEye.tag = "0"
+                    editTextMakePw.transformationMethod = PasswordTransformationMethod.getInstance()
+                    btnEye.setImageResource(R.drawable.icon_eye_on)
+                }
+            }
+            editTextMakePw.setSelection(editTextMakePw!!.length())
+        }
     }
 }
