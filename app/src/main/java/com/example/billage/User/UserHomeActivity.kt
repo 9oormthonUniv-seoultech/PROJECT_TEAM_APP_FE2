ebee9269527a269e7858d5c1a43e7d274285df9d
Geonheu <geonhui0987@gmail.com>
2024-11-05 01:11:03 +0900

    [FEAT] #8 유저 홈 화면 내브바 수정

--- app/src/main/java/com/example/billage/User/UserHomeActivity.kt
+++ app/src/main/java/com/example/billage/User/UserHomeActivity.kt
@@ -21,18 +21,12 @@ class UserHomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityUserHomeBinding.inflate(layoutInflater)
         val toolbar: androidx.appcompat.widget.Toolbar = binding.toolbarUserHome
         setSupportActionBar(toolbar)
 
         setContentView(binding.root)
         setContentView(R.layout.activity_user_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         if(savedInstanceState == null){
             supportFragmentManager.commit {
